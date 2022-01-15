22734941a9c8200e83f74a1383641efed10c5608
TobiasZawada <i@tn-home.de>
2022-01-15 06:50:53 +0100

    Fixes #1: wrong char count for table cell width
    Fixes #1 by overriding buggy `org-string-width`.

--- org+-align-table.el
+++ org+-align-table.el
@@ -34,72 +34,91 @@
 
 (require 'cl-lib)
 (require 'org-table)
-  (defun org+-search-forward-table (&optional limit noerror table-type)
-    "Search for the next org table go to its end and return point.
+(defun org+-search-forward-table (&optional limit noerror table-type)
+  "Search for the next org table go to its end and return point.
   Set the boundaries of group 0 of the match data to the
   beginning and the end of the table.
   LIMIT and NOERROR are like for `search-forward'.
   TABLE-TYPE is the same argument as for `org-at-table-p'."
-    (interactive)
-    (let ((b (org-at-table-p))
-	  (pt (point)))
-      (unless b
-	(while (and
-		(setq b (re-search-forward org-table-any-line-regexp limit noerror))
-		(null (org-at-table-p table-type))))
-	(when
-	    (and (null b) (eq noerror t))
-	  (goto-char pt)))
-      (when b
-	(setq b (org-table-begin))
-	(let ((e (org-table-end table-type)))
+  (interactive)
+  (let ((b (org-at-table-p))
+	(pt (point)))
+    (unless b
+      (while (and
+	      (setq b (re-search-forward org-table-any-line-regexp limit noerror))
+	      (null (org-at-table-p table-type))))
+      (when
+	  (and (null b) (eq noerror t))
+	(goto-char pt)))
+    (when b
+      (setq b (org-table-begin))
+      (let ((e (org-table-end table-type)))
 	(set-match-data (list b e (current-buffer)))
 	(goto-char e)))))
 
-  (defun org+-font-lock-align-table-row ()
-    "Align the table column separators to the actual character columns."
-    (save-excursion
-      (when (org-at-table-p)
-	(beginning-of-line)
-	(skip-chars-forward "[[:space:]]")
-	(forward-char) ;; behind leading ?|
-	(let ((e (line-end-position)))
-	  (while (re-search-forward "[ \t]|" e t)
-	    (put-text-property (match-beginning 0) (1- (match-end 0))
-			       'display
-			       (list 'space :align-to (1- (current-column))))
-	    )))))
+(defun org+-font-lock-align-table-row ()
+  "Align the table column separators to the actual character columns."
+  (save-excursion
+    (when (org-at-table-p)
+      (beginning-of-line)
+      (skip-chars-forward "[[:space:]]")
+      (forward-char) ;; behind leading ?|
+      (let ((e (line-end-position)))
+	(while (re-search-forward "[ \t]|" e t)
+	  (put-text-property (match-beginning 0) (1- (match-end 0))
+			     'display
+			     (list 'space :align-to (1- (current-column))))
+	  )))))
 
-  (defun org+-font-lock-align-tables (limit)
-    "Align one full table."
-    (when (org+-search-forward-table limit 'noerror)
-      (save-match-data
-	(let ((e (match-end 0)))
-	  (goto-char (match-beginning 0))
-	  (while (< (point) e)
-	    (org+-font-lock-align-table-row)
-	    (forward-line))
-	  ))
-      (point)))
+(defun org+-font-lock-align-tables (limit)
+  "Align one full table."
+  (when (org+-search-forward-table limit 'noerror)
+    (save-match-data
+      (let ((e (match-end 0)))
+	(goto-char (match-beginning 0))
+	(while (< (point) e)
+	  (org+-font-lock-align-table-row)
+	  (forward-line))
+	))
+    (point)))
 
-  (defvar org+-align-table-mode-keywords nil)
-  (setq org+-align-table-mode-keywords '((org+-font-lock-align-tables)))
+(defvar org+-align-table-mode-keywords nil)
+(setq org+-align-table-mode-keywords '((org+-font-lock-align-tables)))
 
-  (define-minor-mode org+-align-table-mode
-    "Set up the alignment of table columns."
-    t nil nil
-    (cl-pushnew 'display font-lock-extra-managed-props)
-    (cl-assert (derived-mode-p 'org-mode) "`org+-align-table-mode' is for org-mode only ")
-    (if org+-align-table-mode
-	(font-lock-add-keywords
-	 nil
-	 org+-align-table-mode-keywords
-	 t)
-      (font-lock-remove-keywords
+(defun org+-align-table-string-width (string)
+  "Return length of the visible part of string S.
+Visible part is determined according to text PROPERTY, which is
+either `invisible' or `display'.  BEG and END are 0-indices
+delimiting S."
+  (let ((invisibility-spec buffer-invisibility-spec)
+	(locals (org-get-local-variables)))
+    (with-temp-buffer
+      (setq buffer-invisibility-spec invisibility-spec)
+      (mapcar (lambda (local)
+		(set (car local) (cdr local)))
+	      locals)
+      (insert string)
+      (current-column))))
+
+(advice-add 'org-string-width :override #'org+-align-table-string-width)
+
+(define-minor-mode org+-align-table-mode
+  "Set up the alignment of table columns."
+  t nil nil
+  (cl-pushnew 'display font-lock-extra-managed-props)
+  (cl-assert (derived-mode-p 'org-mode) "`org+-align-table-mode' is for org-mode only ")
+  (if org+-align-table-mode
+      (font-lock-add-keywords
        nil
-       org+-align-table-mode-keywords))
-    (font-lock-flush)
-    (font-lock-ensure))
+       org+-align-table-mode-keywords
+       t)
+    (font-lock-remove-keywords
+     nil
+     org+-align-table-mode-keywords))
+  (font-lock-flush)
+  (font-lock-ensure))
+
+(add-hook 'org-mode-hook 'org+-align-table-mode)
+
 (provide 'org+-align-table)
 ;;; org+-align-table.el ends here
-
